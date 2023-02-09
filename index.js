0f85f4a98def4718f57f938911ef5a27ca300013
Sai <saikiranmsk34@gmail.com>
2023-02-09 17:45:26 +0530

    first commit with complete working

---
+++ index.js
@@ -0,0 +1,64 @@
+var n = document.querySelectorAll("button").length;
+var set = document.querySelectorAll("button");
+for (var i = 0; i < n; i++) {
+  set[i].addEventListener("click", function () {
+    var ch = this.innerText;
+
+    buttonAnimation(ch);
+    makeSound(ch);
+    
+  });
+}
+
+
+document.addEventListener("keydown",function (event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+
+
+function buttonAnimation(ch) {
+  var pressedButton = document.querySelector("." + ch);
+
+  pressedButton.classList.add("pressed");
+
+  setTimeout(function() {
+    pressedButton.classList.remove("pressed");
+  },150);
+
+}
+
+
+function makeSound(ch) {
+  switch (ch) {
+    case "w":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+  }
+}
